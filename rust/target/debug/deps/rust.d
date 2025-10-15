e5ac9af7d38fd956b457500567807f0f2dd92d6d
ChoInHeum <heumi711@gmail.com>
2025-10-16 03:48:54 +0900

    Edit File Structure

--- rust/target/debug/deps/rust.d
+++
@@ -1,5 +0,0 @@
-D:\CBNU\2-2\Programming Language\Project\Rust\rust\target\debug\deps\rust.d: src\main.rs
-
-D:\CBNU\2-2\Programming Language\Project\Rust\rust\target\debug\deps\rust.exe: src\main.rs
-
-src\main.rs:
--- rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-D:\CBNU\2-2\Programming\ Language\Project\Rust\rust\target\debug\rust.exe: D:\CBNU\2-2\Programming\ Language\Project\Rust\rust\src\main.rs
